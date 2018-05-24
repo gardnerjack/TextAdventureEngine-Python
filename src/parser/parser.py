adb88c0fbfb35a3b6f5ae2ff4ab5407659681a32
Jack Gardner <gardnerjack11@gmail.com>
2018-05-24 18:13:49 +1000

    Parser is now a class

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -2,91 +2,94 @@ from re import match, finditer
 from models.Item import Object, Tool
 from models.Location import Location, Destination
 
-locations = {}
-items = {}
+class Parser(object):
 
-def initilise_game_info():
-    initialise_items()
-    initilise_locations()
+    def __init__(self):
+        self.locations = {}
+        self.items = {}
 
-def match_item(pattern, line):
-    obj = match(pattern, line)
-    if obj is not None:
-        value = line[len(pattern) + 1 : len(line) - 1]
-    else:
-        value = None
-    return value
+    def initilise_game_info(self, ):
+        self.initialise_items()
+        self.initilise_locations()
 
-def match_list(pattern, line):
-    obj = match(pattern, line)
-    if obj is not None:
-        items = line[len(pattern) + 1 : len(line) - 1].split(', ')
-    else:
-        items = None
-    return items
+    def match_item(self, pattern, line):
+        obj = match(pattern, line)
+        if obj is not None:
+            value = line[len(pattern) + 1 : len(line) - 1]
+        else:
+            value = None
+        return value
 
-
-def initilise_locations():
-    f = open("game_info/locations.txt", 'r')
-    name, description, loc_items, destinations = (None,) * 4
-
-    for line in f:
-
-        if name is None:
-            name = match_item("name:", line)
-
-        if description is None:
-            description = match_item("description:", line)
-
-        if loc_items is None:
-            loc_items = match_list("items:", line)
-
-        if destinations is None:
-            destinationsObj = match("destinations:", line)
-            if destinationsObj is not None:
-                destinations = [
-                    Destination(dest.group(1), dest.group(2))
-                    for dest
-                    in finditer(r"\"(.+?)\"\[(.+?)\]", line[14 : len(line) - 1])
-                ]
-
-        if match("--", line):
-            items_list = [items[i] for i in loc_items] if loc_items else None
-            locations[name] = Location(name, description, items_list, destinations)
-            name, description, loc_items, destinations = (None,) * 4
-
-    f.close()
+    def match_list(self, pattern, line):
+        obj = match(pattern, line)
+        if obj is not None:
+            self.items = line[len(pattern) + 1 : len(line) - 1].split(', ')
+        else:
+            self.items = None
+        return self.items
 
 
-def initialise_items():
-    f = open("game_info/items.txt", 'r')
-    name, item_type, description, attributes, object_yield = (None,) * 5
+    def initilise_locations(self):
+        f = open("game_info/self.locations.txt", 'r')
+        name, description, loc_items, destinations = (None,) * 4
 
-    for line in f:
+        for line in f:
 
-        if name is None:
-            name = match_item("name:", line)
+            if name is None:
+                name = self.match_item("name:", line)
 
-        if item_type is None:
-            item_type = match_item("type:", line)
+            if description is None:
+                description = self.match_item("description:", line)
 
-        if description is None:
-            description = match_item("description:", line)
+            if loc_items is None:
+                loc_items = self.match_list("self.items:", line)
 
-        if object_yield is None:
-            object_yield = match_item("yield:", line)
+            if destinations is None:
+                destinationsObj = match("destinations:", line)
+                if destinationsObj is not None:
+                    destinations = [
+                        Destination(dest.group(1), dest.group(2))
+                        for dest
+                        in finditer(r"\"(.+?)\"\[(.+?)\]", line[14 : len(line) - 1])
+                    ]
 
-        if attributes is None:
-            attributes = match_list("attributes", line)
+            if match("--", line):
+                self.items_list = [self.items[i] for i in loc_items] if loc_items else None
+                self.locations[name] = Location(name, description, self.items_list, destinations)
+                name, description, loc_items, destinations = (None,) * 4
 
-        if match("--", line):
-            if item_type == 'object':
-                items[name] = Object(name, description, object_yield, attributes)
-            elif item_type == 'tool':
-                items[name] = Tool(name, description, attributes)
-            name, itype, description, attributes, oyield = (None,) * 5
+        f.close()
 
-    f.close()
+
+    def initialise_items(self):
+        f = open("game_info/self.items.txt", 'r')
+        name, item_type, description, attributes, object_yield = (None,) * 5
+
+        for line in f:
+
+            if name is None:
+                name = self.match_item("name:", line)
+
+            if item_type is None:
+                item_type = self.match_item("type:", line)
+
+            if description is None:
+                description = self.match_item("description:", line)
+
+            if object_yield is None:
+                object_yield = self.match_item("yield:", line)
+
+            if attributes is None:
+                attributes = self.match_list("attributes", line)
+
+            if match("--", line):
+                if item_type == 'object':
+                    self.items[name] = Object(name, description, object_yield, attributes)
+                elif item_type == 'tool':
+                    self.items[name] = Tool(name, description, attributes)
+                name, item_type, description, attributes, object_yield = (None,) * 5
+
+        f.close()
 
 
 if __name__ == "__main__":
